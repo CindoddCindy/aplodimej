78756848f1a8fdaa60a0d083d80dd75e1e1103db
CindoddCindy <cindodcindy@gmail.com>
2021-01-21 14:50:41 +0700

    tambah kelas model ajah

---
+++ aplodimejtess/src/main/java/com/aplodimej/aplodimejtess/AplodimejtessApplication.java
@@ -0,0 +1,13 @@
+package com.aplodimej.aplodimejtess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplodimejtessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplodimejtessApplication.class, args);
+	}
+
+}
---
+++ aplodimejtess/src/test/java/com/aplodimej/aplodimejtess/AplodimejtessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplodimej.aplodimejtess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplodimejtessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
